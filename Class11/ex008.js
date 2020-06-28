83bc782896eb8ea78e36a42b4b9e8d16006ed8ed
Felipe Pereira Barros <felipe.pereira.barros@hotmail.com>
2020-06-28 17:54:22 -0300

    condições

---
+++ Class11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A Velocidade do seu carro é ${vel}km por hora`)
+if(vel > 60){
+    console.log('Você ultrapassou o velocidade permitida. MULTADO!')
+} //condição simples
+console.log('Dirija sempre usando seu cinto de segurança')
